deac231e4905318423bf3aa28a0c5b6f66ef8071
Douglas Urner <dlu@canishe.com>
2019-02-28 21:38:03 -0800

    Fix typo & ommissions.

--- new-unity-project.sh
+++ new-unity-project.sh
@@ -2,9 +2,11 @@
 
 REPO=https://github.com/DouglasUrner/Unity-New-Project.git
 
-git clone $REPO .git || exit -1
+git clone $REPO || exit -1
 mv `basename $REPO .git` $1
 rm $1/README.md ; touch $1/README.md
+rm $1/README-*
+rm $1/new-unity-project.sh
 rm -rf $1/.git
 
 mkdir -p $1/Assets/{Materials,Prefabs,Scenes,Scripts}/README.md
